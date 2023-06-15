5769a49c6191f476e20220e0ff0e07c078bfb122
Mulinda Yahaya <mymyahaya@gmail.com>
2023-06-15 12:59:12 +0300

    changes

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,23 +1,18 @@
-// // import express from 'express';
-// // import Product from '../models/productModel.js';
-// // import data from '../data.js';
-// // import User from '../models/userModel.js';
+import express from 'express';
+import Product from '../models/productModel.js';
+import data from '../data.js';
+import User from '../models/userModel.js';
 
-// const express = require('express');
-// const Product = require('../models/productModel.js');
-// const data = require('../data.js');
-// const User = require('../models/userModel.js');
+const seedRouter = express.Router();
 
-// const seedRouter = express.Router();
+seedRouter.get('/', async (req, res) => {
+  await Product.deleteOne({});
+  const createdProducts = await Product.insertMany(data.products);
 
-// seedRouter.get('/', async (req, res) => {
-//   await Product.deleteOne({});
-//   const createdProducts = await Product.insertMany(data.products);
+  await User.deleteOne({});
+  const createdUsers = await User.insertMany(data.users);
 
-//   await User.deleteOne({});
-//   const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
+});
 
-//   res.send({ createdProducts, createdUsers });
-// });
-
-// export default seedRouter;
+export default seedRouter;
